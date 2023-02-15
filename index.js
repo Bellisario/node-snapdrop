3c04c563fcb0b0acb849e7c03931c0912f1f8c41
Giorgio Bellisario <72039923+Bellisario@users.noreply.github.com>
2023-02-15 20:43:20 +0100

    fix: rate limit ip address match
    Co-authored-by: schlagmichdoch  <schlagmichdoch@users.noreply.github.com>

--- index.js
+++ index.js
@@ -43,6 +43,10 @@ const publicRun = process.argv[2];
 
 app.use(limiter);
 
+// ensure correct client ip and not the ip of the reverse proxy is used for rate limiting on render.com
+// see https://github.com/express-rate-limit/express-rate-limit#troubleshooting-proxy-issues
+app.set('trust proxy', 5);
+
 app.use(express.static('public'));
 
 app.use(function(req, res) {
